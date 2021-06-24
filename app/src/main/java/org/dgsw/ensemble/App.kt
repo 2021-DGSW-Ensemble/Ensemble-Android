1ae8eab30e8f8e32b11caa63321a871d16762824
KimJinHo6831 <52987098+KimJinHo6831@users.noreply.github.com>
2021-06-24 16:50:08 +0900

    Add VideoPlayer

--- app/src/main/java/org/dgsw/ensemble/App.kt
+++ app/src/main/java/org/dgsw/ensemble/App.kt
@@ -2,12 +2,12 @@ package org.dgsw.ensemble
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import org.dgsw.ensemble.di.component.AppComponent
+//import org.dgsw.ensemble.di.component.AppComponent
 
 @HiltAndroidApp
 class App : Application() {
 
-    lateinit var appComponent: AppComponent
+    //lateinit var appComponent: AppComponent
 
     override fun onCreate() {
         super.onCreate()
